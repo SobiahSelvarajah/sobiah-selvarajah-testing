31d1fe7c672cc5da3b6a5d083e7419fcbf120e10
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-05-11 01:16:25 +0100

    script.py - code from guessing game added

---
+++ script.py
@@ -0,0 +1,15 @@
+import random
+
+answer = random.randint(1, 10)
+while True:
+    try:
+        guess = int(input('guess a number from 1~10:  '))
+        if 0 < guess < 11:
+            if guess == answer:
+                print('Well done, perfect guess!')
+                break
+            else:
+                print('Please try again')
+    except ValueError:
+        print('please enter a number')
+        continue
