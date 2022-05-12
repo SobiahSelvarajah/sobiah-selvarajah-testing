cfaaa370ac92dbcf6a6bc01cf55e295ce832e94f
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-05-13 00:12:06 +0200

    test_script.py - stuck on break

--- script.py
+++ script.py
@@ -1,17 +1,32 @@
 import random
 
-answer = random.randint(1, 10)
-while True:
-    try:
-        guess = int(input('guess a number from 1~10:  '))
-        if 0 < guess < 11:
-            if guess == answer:
-                print('Well done, perfect guess!')
-                break
-            else:
-                print('Please try again')
-    except ValueError:
-        print('please enter a number')
-        continue
+answer = random.randint(1,10)
+
+def obtain_guess(guess):
+    if 0 < guess < 11:
+        if guess == answer:
+            print('Well done, perfect guess!')
+            break
+        else:
+            print('Please try again')
+
+
+
+
+# import random
+#
+# answer = random.randint(1, 10)
+# while True:
+#     try:
+#         guess = int(input('guess a number from 1~10:  '))
+#         if 0 < guess < 11:
+#             if guess == answer:
+#                 print('Well done, perfect guess!')
+#                 break
+#             else:
+#                 print('Please try again')
+#     except ValueError:
+#         print('please enter a number')
+#         continue
 
 
