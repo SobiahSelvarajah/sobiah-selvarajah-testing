fa29cadf3091e3b776ee0f726d83342b3cfcd463
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2022-05-26 13:17:45 +0100

    test_script.py - tests added

--- test_script.py
+++ test_script.py
@@ -4,7 +4,20 @@ import script
 
 class TestGame(unittest.TestCase):
     def test_input(self):
-        pass
+        result = script.obtain_guess(5, 5)
+        self.assertTrue(result)
+
+    def test_input_wrong_guess(self):
+        result = script.obtain_guess(5, 0)
+        self.assertFalse(result)
+
+    def test_input_wrong_number(self):
+        result = script.obtain_guess(5, 11)
+        self.assertFalse(result)
+
+    def test_input_wrong_type(self):
+        result = script.obtain_guess(5, '11')
+        self.assertFalse(result)
 
 
 if __name__ == '__main__':
